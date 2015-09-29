0027d77c7a31351a98e4aeb5d7780987f755a686
Hadiya Hope-Crichlow <hopecrichlow@live.com>
2015-09-29 16:56:32 -0400

    Basic calculator complete

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+// Grab My Elements
+var firstInput = document.querySelector('#num_one');
+var secondInput = document.querySelector('#num_two');
+var addIt = document.querySelector('#calculateBtn');
+var sumElem = document.querySelector('#answer');
+
+// Variable Declarations
+var a, b;
+
+// Function to Perform the Answer
+var calculateBtn = function () {
+  a = firstInput.value;
+  b = secondInput.value;
+  answerText = (Number(a) + Number(b));
+  sumElem.textContent = answerText;
+};
+
+// Button Click
+addIt.addEventListener('click', calculateBtn);
